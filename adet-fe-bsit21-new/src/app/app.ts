22b02a8026cb8c28ea68bafba570ce205a49c2a1
RonMichaelDeDeloso <rdeloso12002@liceo.edu.ph>
2026-02-03 22:21:57 +0800

    activity blog poste

--- adet-fe-bsit21-new/src/app/app.ts
+++ adet-fe-bsit21-new/src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('adet-fe-bsit21');
+  protected readonly title = signal('adet-fe-bsit21-new');
 }
